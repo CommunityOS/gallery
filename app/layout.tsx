dad79e28aa2f1d2a382b243cdbe8528689cdf92e
Ignacio Guzmán <ignacio658mg@gmail.com>
2024-09-01 02:23:34 -0400

    feat: updating favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,6 @@ export default function RootLayout({
     <ShallowRoutingProvider>
       <html lang="es">
         <head>
-          <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
